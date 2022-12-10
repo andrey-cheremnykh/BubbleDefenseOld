b5a14a880ba5165a904b141206a126ac769f99d9
Andrey <Andrey@DESKTOP-NI1F48K>
2022-12-10 19:16:53 +0200

    Lesson 70 2

--- Assets/Scripts/GUI/GaneOverPanel.cs
+++ Assets/Scripts/GUI/GaneOverPanel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GaneOverPanel : MonoBehaviour
 {
-    [SerializeField] GameObject losePanel
+    [SerializeField] GameObject losePanel;
     // Start is called before the first frame update
     void Start()
     {
